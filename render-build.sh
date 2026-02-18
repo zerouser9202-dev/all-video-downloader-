6a65fd10ae535fff4cb99b12d60297a900e35d4f
zero <ffzero8434@gmail.com>
2026-02-18 23:18:03 +0530

    remove cookies for cloud test

--- render-build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o yt-dlp
-chmod +x yt-dlp
-
-curl -L https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz -o ffmpeg.tar.xz
-tar -xf ffmpeg.tar.xz
-mv ffmpeg-*-amd64-static/ffmpeg ./ffmpeg
-chmod +x ffmpeg
